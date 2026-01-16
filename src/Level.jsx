eb7f2fa8b7bb987d82b9821305b3fd84e9caf591
siddhant <siddhantsin343@gmail.com>
2026-01-16 22:29:40 +0530

    blockEnd added

--- src/Level.jsx
+++ src/Level.jsx
@@ -24,6 +24,22 @@ function BlockStart({ position = [0, 0, 0] }) {
   );
 }
 
+
+function BlockEnd({ position = [0, 0, 0] }) {
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        scale={[4, -0.2, 4]}
+        receiveShadow
+      />
+    </group>
+  );
+}
+
+
 function BlockSpinner({ position = [0, 0, 0] }) {
   const obstacle = useRef();
   const [speed] = useState(
@@ -153,10 +169,11 @@ function BlockAxe({ position = [0, 0, 0] }) {
 function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 12]} />
-      <BlockSpinner position={[0, 0, 8]} />
-      <BlockLimbo position={[0, 0, 4]} />
-      <BlockAxe position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 16]} />
+      <BlockSpinner position={[0, 0, 12]} />
+      <BlockLimbo position={[0, 0, 8]} />
+      <BlockAxe position={[0, 0, 4]} />
+      <BlockEnd position={[0, 0, 0]} />
     </>
   );
 }
