6ba0cecbb6edafa885fe69a8ec862b4b9cd1052a
siddhant <siddhantsin343@gmail.com>
2026-01-17 14:58:58 +0530

    floor is updated with cuboidColliders and play area done.

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useMemo } from "react";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
-import { RigidBody } from "@react-three/rapier";
+import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import { useGLTF } from "@react-three/drei";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -186,6 +186,7 @@ function Bounds({ length = 1 }) {
       <mesh position={[2.15, 0.75, - (length*2)+ 2]} geometry={boxGeometry} material={wallMaterial} scale={[0.3, 1.5, 4 * length]} castShadow />
       <mesh position={[-2.15, 0.75, - (length*2)+ 2]} geometry={boxGeometry} material={wallMaterial} scale={[0.3, 1.5, 4 * length]} receiveShadow />
       <mesh position={[0, 0.75, -(length * 4) +2]} geometry={boxGeometry} material={wallMaterial} scale={[4, 1.5, 0.3]} receiveShadow />
+      <CuboidCollider args={[2,0.1,2 * length]} position={[0, -0.1, -(length*2)+2]} restitution={0.2} friction={1}/>
     </RigidBody>
 
 
